f0997b5acc6f82813e38a1f43ed9f400817b99ba
joel <vashappnin107@gmail.com>
2026-01-05 23:52:07 +0530

    add asset type

--- models/holdingsModel.go
+++ models/holdingsModel.go
@@ -2,12 +2,20 @@ package models
 
 import "time"
 
+type HoldingType string
+
+const (
+	STOCK HoldingType = "STOCK"
+	CRYPTO HoldingType = "CRYPTO"
+)
+
 type Holding struct {
 	ID          uint      `json:"id" gorm:"primaryKey"`
 	WalletID    uint      `json:"wallet_id" gorm:"not null;uniqueIndex:idx_wallet_symbol"`
 	Symbol      string    `json:"symbol" gorm:"not null;uniqueIndex:idx_wallet_symbol"`
 	Quantity    uint64    `json:"quantity" gorm:"not null"`
 	AvgBuyPrice uint64    `json:"avg_buy_price" gorm:"not null"`
+	Type        HoldingType `json:"type" gorm:"type:varchar(10);not null"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
