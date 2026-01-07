44cf303f9f51bf62f800c0b21119a03e1dc16338
joel <vashappnin107@gmail.com>
2026-01-08 03:58:13 +0530

    sell crypto func

--- handlers/tradeHandler.go
+++ handlers/tradeHandler.go
@@ -178,3 +178,118 @@ func BuyHandler(c *fiber.Ctx) error {
 		"quantity":  float64(holding.Quantity) / 1e8,
 	})
 }
+
+func SellHandler(c *fiber.Ctx) error {
+	symbol := strings.ToUpper(c.Params("symbol"))
+	log.Println("symbol", symbol)
+
+	// Parse quantity as float first, then convert to satoshis
+	qtyFloat, err := strconv.ParseFloat(c.Params("quantity"), 64)
+	if err != nil || qtyFloat <= 0 {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid quantity"})
+	}
+
+	// Convert quantity to satoshis (e.g., 0.5 BTC = 50000000 satoshis) i.e 1^8
+	qty := uint64(qtyFloat * 1e8)
+
+	userID, ok := c.Locals("user_id").(uint)
+	if !ok {
+		return c.Status(401).JSON(fiber.Map{"error": "Unauthorized"})
+	}
+
+	price, err := MarketPrice(symbol)
+	if err != nil {
+		return c.Status(503).JSON(fiber.Map{"error": "Market unavailable"})
+	}
+
+	// Calculate total sale
+	// qty in satoshi_BTC, price in satoshi_USDT per satoshi_BTC
+	totalSale := qty * price
+
+	tx := database.Database.Db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	//verify wallet exists
+	var wallet models.Wallet
+	log.Println("userid", userID)
+	if err := tx.
+		Clauses(clause.Locking{Strength: "UPDATE"}).
+		Where("user_id = ?", userID).
+		First(&wallet).Error; err != nil {
+		tx.Rollback()
+		return c.Status(404).JSON(fiber.Map{"error": "Wallet not found"})
+	}
+
+	//verify holding exists
+	var holding models.Holding
+	//lock the row to prevent double spending
+
+	if err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Where("wallet_id = ? AND symbol = ?", wallet.ID, symbol).First(&holding).Error; err != nil {
+		tx.Rollback()
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return c.Status(400).JSON(fiber.Map{"error": "You do not own this asset"})
+		}
+		return c.Status(404).JSON(fiber.Map{"error": "Database error"})
+	}
+
+	//check if user has enuf quantity to sell
+	if holding.Quantity < qty {
+		tx.Rollback()
+		return c.Status(422).JSON(fiber.Map{"error": "Insufficient asset quantity"})
+	}
+
+	//calculate pnl
+	pnl := int64(((int64(price) - int64(holding.AvgBuyPrice)) * int64(qty)) / 100000000)
+
+	//update holding quantity
+	holding.Quantity -= qty
+	//if quantity is zeeo delete holding
+	if holding.Quantity == 0 {
+		if err := tx.Delete(&holding).Error; err != nil {
+			tx.Rollback()
+			return c.SendStatus(500)
+		}
+	} else {
+		if err := tx.Save(&holding).Error; err != nil {
+			tx.Rollback()
+			return c.SendStatus(500)
+		}
+	}
+
+	//update wallet balance
+	wallet.Balance += totalSale
+	if err := tx.Save(&wallet).Error; err != nil {
+		tx.Rollback()
+		return c.Status(500).JSON(fiber.Map{"error": "Couldnt update wallet balance"})
+	}
+
+	//create transaction record w pnl
+	trade := models.Transaction{
+		WalletID:     wallet.ID,
+		Symbol:       symbol,
+		Type:         models.Sell,
+		Quantity:     qty,
+		PricePerUnit: price,
+		TotalAmount:  totalSale,
+		RealizedPnL:  pnl,
+	}
+	if err := tx.Create(&trade).Error; err != nil {
+		tx.Rollback()
+		return c.SendStatus(500)
+	}
+	if err := tx.Commit().Error; err != nil {
+		return c.SendStatus(500)
+	}
+
+	return c.JSON(fiber.Map{
+		"status":             "success",
+		"new_balance":        float64(wallet.Balance) / 1e8, // Convert back for display
+		"pnl":                float64(pnl),
+		"sold_at":            float64(price),
+		"remaining_quantity": float64(holding.Quantity) / 1e8,
+	})
+}
