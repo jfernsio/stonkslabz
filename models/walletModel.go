cde7660579fa6607ef33c84a4d59327bb647086f
joel <vashappnin107@gmail.com>
2026-01-07 23:59:03 +0530

    add satoshi units n pnl

--- models/walletModel.go
+++ models/walletModel.go
@@ -5,7 +5,7 @@ import "time"
 type Wallet struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	UserID    uint      `json:"user_id" gorm:"not null;index"`
-	Balance   uint64    `json:"balance" gorm:"not null;default:100000"`
+	Balance   uint64    `json:"balance" gorm:"not null;default:10000000000000"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 
