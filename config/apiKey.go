5e3d425a9c5a472b430e0c7cf6d2e66c8a5e38af
joel <vashappnin107@gmail.com>
2026-02-23 08:40:37 +0530

    add remaning api keys

--- config/apiKey.go
+++ config/apiKey.go
@@ -5,19 +5,21 @@ import (
 )
 
 type Config struct {
-	CoinGecko string
-	FMP       string
-	FinHub    string
-	Alpha     string
-	Twele     string
+	CoinGecko   string
+	FMP         string
+	FinHub      string
+	Alpha       string
+	Twele       string
+	TweleCandle string
 }
 
 func LoadConfig() *Config {
 	return &Config{
-		CoinGecko: os.Getenv("CoinGecko"),
-		FMP:       os.Getenv("FMP"),
-		FinHub:    os.Getenv("FINHUB"),
-		Alpha:     os.Getenv("ALPHAVANTAGE"),
-		Twele:     os.Getenv("TWELE_DATA"),
+		CoinGecko:   os.Getenv("CoinGecko"),
+		FMP:         os.Getenv("FMP"),
+		FinHub:      os.Getenv("FINHUB"),
+		Alpha:       os.Getenv("ALPHAVANTAGE"),
+		Twele:       os.Getenv("TWELE_DATA"),
+		TweleCandle: os.Getenv("TWELE_DATA_CANDLES"),
 	}
 }
