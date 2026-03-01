8e5372b7d0387ee3b7c712a9c54e29b3ba455184
joel <vashappnin107@gmail.com>
2026-03-01 20:36:32 +0530

    update model to use proper index constraint

--- models/watchListModel.go
+++ models/watchListModel.go
@@ -4,8 +4,8 @@ import "time"
 
 type Watchlist struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	UserID    uint      `json:"user_id" gorm:"not null;idx_user_symbol,unique"`
-	Symbol    string    `json:"symbol" gorm:"not null;index:idx_user_symbol,unique"`
+	UserID    uint      `json:"user_id" gorm:"not null;uniqueIndex:idx_user_symbol"`
+	Symbol    string    `json:"symbol" gorm:"not null;uniqueIndex:idx_user_symbol"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
